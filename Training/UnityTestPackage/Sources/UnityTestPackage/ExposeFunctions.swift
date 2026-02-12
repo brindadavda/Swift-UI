2d2e2be469d90e234b9203ff35081aeab2a2431b
davdabrinda <davdabrinda@gmail.com>
2026-02-12 15:30:54 +0530

    added UnityTestPackage

---
+++ Training/UnityTestPackage/Sources/UnityTestPackage/ExposeFunctions.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  UnityTestPackage
+//
+//  Created by Brinda Davda on 22/02/25.
+//
+
+import Foundation
+
+@_cdecl("runNativeCode")
+public func runNativeCode(){
+    NativeIOSCdoe.runNativeCode()
+}
---
+++ Training/UnityTestPackage/Sources/UnityTestPackage/UnityTestPackage.swift
@@ -0,0 +1,9 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+
+public struct NativeIOSCdoe{
+    static func runNativeCode(){
+        print("Running native ios code..")
+    }
+}
